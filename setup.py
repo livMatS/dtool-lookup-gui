d895e51c30b5df66ad26782db53a02f5cd9c03fa
Antoine Sanner <40853209+AntoineSIMTEK@users.noreply.github.com>
2020-11-09 08:16:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -56,8 +56,9 @@ setup(
         'aiohttp>=3.6',
         'gbulb>=0.6',
         'pyyaml>=5.3',
-        'PyGObject>=3.36'
-        'scipy>=1.5'
+        'PyGObject>=3.36',
+        'scipy>=1.5',
+        'numpy'
     ],
     setup_requires=[
         'setuptools_scm>=3.5.0'
