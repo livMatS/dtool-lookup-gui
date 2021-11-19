c4cf4da2da1429913c9c4591a6b16a8a8dcda6b7
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-11-19 10:21:13 +0100

    MAINT: Added configuration button to list of endpoints

--- dtool_lookup_gui/SettingsDialog.py
+++ dtool_lookup_gui/SettingsDialog.py
@@ -24,6 +24,8 @@
 
 import asyncio
 
+from gi.repository import Gtk
+
 from dtool_lookup_api.core.config import Config
 from dtool_lookup_api.core.LookupClient import authenticate
 
@@ -64,6 +66,38 @@ class SignalHandler:
             self.builder.get_object('token-entry').set_text(Config.token)
         if Config.auth_url is not None:
             self.builder.get_object('authenticator-url-entry').set_text(Config.auth_url)
+        asyncio.create_task(self._refresh_list_of_endpoints())
+
+    async def _refresh_list_of_endpoints(self):
+        endpoints_list_box = self.builder.get_object('endpoints-list-box')
+        for child in endpoints_list_box.get_children():
+            child.destroy()
+        base_uris = await self.main_application.lookup_tab.lookup.list_base_uris()
+        first_row = None
+        for base_uri in base_uris:
+            print(base_uri["base_uri"])
+            row = Gtk.ListBoxRow()
+            if first_row is None:
+                first_row = row
+            hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+            hbox.set_margin_top(12)
+            hbox.set_margin_bottom(12)
+            hbox.set_margin_start(12)
+            hbox.set_margin_end(12)
+            vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+            label = Gtk.Label(xalign=0)
+            label.set_markup(f'<b>{base_uri["base_uri"]}</b>')
+            vbox.pack_start(label, True, True, 0)
+            label = Gtk.Label(xalign=0)
+            label.set_markup('This endpoint was reported by the lookup server.')
+            vbox.pack_start(label, True, True, 0)
+            hbox.pack_start(vbox, True, True, 0)
+            button = Gtk.Button(image=Gtk.Image.new_from_icon_name('emblem-system-symbolic', Gtk.IconSize.BUTTON))
+            hbox.pack_end(button, False, False, 0)
+            row.add(hbox)
+            endpoints_list_box.add(row)
+        endpoints_list_box.select_row(first_row)
+        endpoints_list_box.show_all()
 
     def on_settings_window_delete(self, widget, event):
         # Don't delete, simply hide the window
@@ -95,6 +129,7 @@ class SignalHandler:
             self.main_application.show_error(str(e))
             return
         self.builder.get_object('token-entry').set_text(token)
+        await self._refresh_list_of_endpoints()
 
     def on_auth_ok_clicked(self, widget):
         self.auth_dialog.hide()
@@ -103,6 +138,9 @@ class SignalHandler:
             self.builder.get_object('username-entry').get_text(),
             self.builder.get_object('password-entry').get_text()
         ))
+
+        # Reconnect since settings may have been changed
+        asyncio.create_task(self.main_application.lookup_tab.connect())
 
     def on_auth_cancel_clicked(self, widget):
         self.auth_dialog.hide()
