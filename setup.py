8e726e77efc33d8fdf4807630cc69c4aa01481af
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2021-11-19 01:25:56 +0100

    MAINT: requirements, doc

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     use_scm_version=True,
     install_requires=[
         'dtoolcore>=3.17',
-        'dtool-gui-tl>=0.4',
+        'dtool-gui-tk>=0.4',
         'dtool-lookup-api>=0.1',
         'aiohttp>=3.6',
         'gbulb>=0.6',
