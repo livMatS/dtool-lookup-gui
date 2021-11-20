a79ead631da26bde187cd6830121e9683b54b9a3
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-11-20 21:56:06 +0100

    ENH: Option to add endpoints in settings dialog

--- dtool_lookup_gui/SettingsDialog.py
+++ dtool_lookup_gui/SettingsDialog.py
@@ -26,7 +26,7 @@ import asyncio
 
 from gi.repository import Gtk
 
-from dtoolcore.utils import get_config_value, write_config_value_to_file, generous_parse_uri
+from dtoolcore.utils import get_config_value, write_config_value_to_file, generous_parse_uri, _get_config_dict_from_file
 from dtool_lookup_api.core.config import Config
 from dtool_lookup_api.core.LookupClient import authenticate
 
@@ -66,9 +66,19 @@ class SignalHandler:
         for child in endpoints_list_box.get_children():
             child.destroy()
         base_uris = await self.main_application.lookup_tab.lookup.list_base_uris()
+        base_uris = {base_uri['base_uri']: base_uri | {'local': False, 'remote': True} for base_uri in base_uris}
 
-        for base_uri in base_uris:
-            parsed_uri = generous_parse_uri(base_uri["base_uri"])
+        config_dict = _get_config_dict_from_file()
+        for key, value in config_dict.items():
+            if key.startswith('DTOOL_S3_ENDPOINT'):
+                base_uri = f's3://{key[18:]}'
+                if base_uri not in base_uris:
+                    base_uris[base_uri] = {'local': True, 'remote': False}
+                else:
+                    base_uris[base_uri]['local'] = True
+
+        for base_uri, info in base_uris.items():
+            parsed_uri = generous_parse_uri(base_uri)
             row = Gtk.ListBoxRow()
             hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
             hbox.set_margin_top(12)
@@ -77,13 +87,15 @@ class SignalHandler:
             hbox.set_margin_end(12)
             vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
             label = Gtk.Label(xalign=0)
-            label.set_markup(f'<b>{base_uri["base_uri"]}</b>')
+            label.set_markup(f'<b>{base_uri}</b>')
             vbox.pack_start(label, True, True, 0)
             label = Gtk.Label(xalign=0)
-            if _is_configured(parsed_uri):
-                label.set_markup('This endpoint was reported by the lookup server and it is configured locally.')
+            if info['local'] and info['remote']:
+                label.set_markup('This endpoint is reported by the lookup server and it is configured locally.')
+            elif not info['local'] and info['remote']:
+                label.set_markup('This endpoint is reported by the lookup server, but it is not configured locally.')
             else:
-                label.set_markup('This endpoint was reported by the lookup server, but it is not configured locally')
+                label.set_markup('This endpoint is configured locally.')
             vbox.pack_start(label, True, True, 0)
             hbox.pack_start(vbox, True, True, 0)
             button = Gtk.Button(image=Gtk.Image.new_from_icon_name('emblem-system-symbolic', Gtk.IconSize.BUTTON))
@@ -95,13 +107,14 @@ class SignalHandler:
             row.add(hbox)
             endpoints_list_box.add(row)
 
+        # Plus button for adding new endpoints
         row = Gtk.ListBoxRow()
         image = Gtk.Image.new_from_icon_name('list-add-symbolic', Gtk.IconSize.BUTTON)
         image.set_margin_top(20)
         image.set_margin_bottom(20)
         image.set_margin_start(12)
         image.set_margin_end(12)
-        row.connect('state-changed', self.on_add_endpoint_clicked)
+        row.connect('state-changed', self.on_add_endpoint_state_changed)
         row.add(image)
         endpoints_list_box.add(row)
 
@@ -153,6 +166,27 @@ class SignalHandler:
     def on_auth_cancel_clicked(self, widget):
         self.auth_dialog.hide()
 
+    def _edit_endpoint(self, s3_bucket=None, s3_endpoint='', s3_access_key='', s3_secret_key='', s3_prefix=''):
+        s3_bucket_entry = self.builder.get_object('s3-bucket-entry')
+        if s3_bucket is None:
+            # This is a new endpoint; we can edit the bucket name
+            s3_bucket_entry.set_text('')
+            s3_bucket_entry.set_sensitive(True)
+        else:
+            # This is an existing endpoint; we do not allow editing the bucket name
+            s3_bucket_entry.set_text(s3_bucket)
+            s3_bucket_entry.set_sensitive(False)
+        if s3_endpoint is not None:
+            self.builder.get_object('s3-endpoint-url-entry').set_text(s3_endpoint)
+        if s3_access_key is not None:
+            self.builder.get_object('s3-access-key-entry').set_text(s3_access_key)
+        if s3_secret_key is not None:
+            self.builder.get_object('s3-secret-key-entry').set_text(s3_secret_key)
+        if s3_prefix is not None:
+            self.builder.get_object('s3-prefix-entry').set_text(s3_prefix)
+
+        self.s3_configuration_dialog.show()
+
     def on_configure_endpoint_clicked(self, widget):
         base_uri = widget.base_uri
         if base_uri.scheme == 's3':
@@ -160,33 +194,27 @@ class SignalHandler:
             s3_access_key = get_config_value(f'DTOOL_S3_ACCESS_KEY_ID_{base_uri.netloc}')
             s3_secret_key = get_config_value(f'DTOOL_S3_SECRET_ACCESS_KEY_{base_uri.netloc}')
             s3_prefix = get_config_value(f'DTOOL_S3_DATASET_PREFIX')
-            if s3_endpoint:
-                self.builder.get_object('s3-endpoint-url-entry').set_text(s3_endpoint)
-            if s3_access_key:
-                self.builder.get_object('s3-access-key-entry').set_text(s3_access_key)
-            if s3_secret_key:
-                self.builder.get_object('s3-secret-key-entry').set_text(s3_secret_key)
-            if s3_prefix:
-                self.builder.get_object('s3-prefix-entry').set_text(s3_prefix)
 
-            self.s3_configuration_dialog.base_uri = base_uri
-            self.s3_configuration_dialog.show()
+            self._edit_endpoint(base_uri.netloc, s3_endpoint, s3_access_key, s3_secret_key, s3_prefix)
 
     def on_add_endpoint_state_changed(self, widget, state):
-        print('add endpoint', widget, state)
+        if state == Gtk.StateType.ACTIVE:
+            self._edit_endpoint()
 
     def on_s3_configuration_apply_clicked(self, widget):
         self.s3_configuration_dialog.hide()
 
-        base_uri = self.s3_configuration_dialog.base_uri
-        write_config_value_to_file(f'DTOOL_S3_ENDPOINT_{base_uri.netloc}',
+        bucket_name = self.builder.get_object('s3-bucket-entry').get_text()
+        write_config_value_to_file(f'DTOOL_S3_ENDPOINT_{bucket_name}',
                                    self.builder.get_object('s3-endpoint-url-entry').get_text())
-        write_config_value_to_file(f'DTOOL_S3_ACCESS_KEY_ID_{base_uri.netloc}',
+        write_config_value_to_file(f'DTOOL_S3_ACCESS_KEY_ID_{bucket_name}',
                                    self.builder.get_object('s3-access-key-entry').get_text())
-        write_config_value_to_file(f'DTOOL_S3_SECRET_ACCESS_KEY_{base_uri.netloc}',
+        write_config_value_to_file(f'DTOOL_S3_SECRET_ACCESS_KEY_{bucket_name}',
                                    self.builder.get_object('s3-secret-key-entry').get_text())
         write_config_value_to_file(f'DTOOL_S3_DATASET_PREFIX',
                                    self.builder.get_object('s3-prefix-entry').get_text())
+
+        asyncio.create_task(self._refresh_list_of_endpoints())
 
     def on_s3_configuration_cancel_clicked(self, widget):
         self.s3_configuration_dialog.hide()
