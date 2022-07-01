e53959e02da0a2e790c38d2bf623a7aa8c37d831
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2022-07-01 14:05:12 +0200

    MAINT: Updated Linux launcher setup to default flat hierarchy in build package

--- maintenance/set_launcher_icon.sh
+++ maintenance/set_launcher_icon.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -e
-
 #
 # Run this to update the launcher file with the current path to the application icon
 #
 
-APPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-if [ -w "$APPDIR"/etc/dtool-lookup-gui.desktop ]; then
-	sed -i -e "s@^Icon=.*@Icon=$APPDIR/data/icons/256x256/dtool_logo.xpm@" "$APPDIR"/etc/dtool-lookup-gui.desktop
+APPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ -w "$APPDIR"/dtool-lookup-gui.desktop ]; then
+	sed -i -e "s@^Icon=.*@Icon=$APPDIR/dtool_logo.png@" "$APPDIR"/dtool-lookup-gui.desktop
 else
-	echo "$APPDIR"/etc/dtool-lookup-gui.desktop is not writable
+	echo "$APPDIR"/dtool-lookup-gui.desktop is not writable
 	exit 1
 fi
--- maintenance/soft_link_launcher.sh
+++ maintenance/soft_link_launcher.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
 # Place link to desktop launcher within appropriate place below home folder
+# Expects dtool-lookup-gui executable to reside within same directory as this script.
+# Expects dtool-lookup-gui.desktop launcher to reside within same directory as this script.
 #
-APPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+APPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p "${HOME}/.local/share/applications"
-ln -sf "${APPDIR}/etc/dtool-lookup-gui.desktop" "${HOME}/.local/share/applications/dtool-lookup-gui.desktop"
+ln -sf "${APPDIR}/dtool-lookup-gui.desktop" "${HOME}/.local/share/applications/dtool-lookup-gui.desktop"
