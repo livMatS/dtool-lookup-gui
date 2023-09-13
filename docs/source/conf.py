bd878b095d0cc9c323c2aa464251673d1c01b605
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2023-09-13 16:58:06 +0200

    DOC: provide relative images

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,22 @@
 import os
 import sys
 
+# -- Prepare
+
+import shutil
+import os
+
+# Define the source and destination paths for images
+data_source_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'data')
+data_destination_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+# Copy images from source to destination
+shutil.rmtree(data_destination_dir, ignore_errors=True)  # Remove existing destination directory
+shutil.copytree(data_source_dir, data_destination_dir)  # Copy images
+
+# Add the new image path to the HTML static path
+# html_static_path.append(data_destination_dir)
+
 root = os.path.dirname(sys.argv[0])
 
 def read_authors(fn):
@@ -50,3 +66,8 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This patterns also effect to html_static_path and html_extra_path
+exclude_patterns = ['data']
