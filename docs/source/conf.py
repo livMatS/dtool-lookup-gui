10d55736dcc2846e94102bc71c0a566f74086663
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2023-09-13 17:08:02 +0200

    DOC: modified sphinx conf for rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,11 @@ import sys
 import shutil
 import os
 
+root = os.path.dirname(__file__)
+
 # Define the source and destination paths for images
-data_source_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'data')
-data_destination_dir = os.path.join(os.path.dirname(__file__), 'data')
+data_source_dir = os.path.join(root, '..', '..', 'data')
+data_destination_dir = os.path.join(root, 'data')
 
 # Copy images from source to destination
 shutil.rmtree(data_destination_dir, ignore_errors=True)  # Remove existing destination directory
@@ -18,13 +20,13 @@ shutil.copytree(data_source_dir, data_destination_dir)  # Copy images
 # Add the new image path to the HTML static path
 # html_static_path.append(data_destination_dir)
 
-root = os.path.dirname(sys.argv[0])
+# root = os.path.dirname(sys.argv[0])
 
 def read_authors(fn):
     return {email.strip('<>'): name for name, email in
             [line.rsplit(maxsplit=1) for line in open(fn, 'r')]}
 
-authors = read_authors('{}/../../AUTHORS'.format(root))
+authors = read_authors(os.path.join(root, '..', '..', 'AUTHORS'))
 
 authors_alphabetical = sorted(list(set(authors.values())))
 
