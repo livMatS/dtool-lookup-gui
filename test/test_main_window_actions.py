509f9cf2e72184ea29595a3169f3698328d2a0a6
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2023-11-27 13:06:34 +0100

    TST: some comments on how unit tests could look like

--- test/test_main_window_actions.py
+++ test/test_main_window_actions.py
@@ -11,8 +11,15 @@ async def test_do_refresh_view(app):
     # Actually activate the untempered method with all side effects
     app.main_window.do_refresh_view(None, None)
 
+    # We could check for any effects of the action here, e.g.
+    # have Gtk widgets been  populated with teh right data?
+
     # Patch the main window's refresh method and make sure it's called when
     # action activated via the Gtk framework
     with patch.object(app.main_window, 'refresh', new_callable=Mock) as mock_refresh:
         app.main_window.activate_action('refresh-view')
         mock_refresh.assert_called_once()
+
+        # We could replace the refresh mock with mocks of other calls
+        # embedded deeper in the hierarchy and assert that the call
+        # hierarchy is preserved as we expect it
