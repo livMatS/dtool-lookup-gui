2bf5656031f356be0458101a4c93bf3986416072
ashdriod <ashwinvazhappilly@gmail.com>
2023-11-30 02:28:58 +0100

    Actual call working

--- test/test_main_window_actions.py
+++ test/test_main_window_actions.py
@@ -1,25 +1,61 @@
-from unittest.mock import patch, MagicMock, create_autospec, Mock
 import pytest
-from gi.repository import Gtk
+from unittest.mock import patch, Mock, MagicMock
+import asyncio
+from gi.repository import Gtk, Gio, GLib
 from dtool_lookup_gui.views.main_window import MainWindow
+from unittest.mock import patch, MagicMock, AsyncMock
+
+
 
 
 @pytest.mark.asyncio
 async def test_do_refresh_view(app):
     """Test the do_refresh_view method triggers the refresh method."""
-    
-    # Actually activate the untempered method with all side effects
+
+    # Mock dependencies
+    mock_dataset_list_box = MagicMock()
+    app.main_window.dataset_list_box = mock_dataset_list_box
+
+
     app.main_window.do_refresh_view(None, None)
+    # Assertions for side effects (if applicable)
 
-    # We could check for any effects of the action here, e.g.
-    # have Gtk widgets been  populated with teh right data?
 
-    # Patch the main window's refresh method and make sure it's called when
-    # action activated via the Gtk framework
+    # Patch the main window's refresh method
     with patch.object(app.main_window, 'refresh', new_callable=Mock) as mock_refresh:
         app.main_window.activate_action('refresh-view')
         mock_refresh.assert_called_once()
 
-        # We could replace the refresh mock with mocks of other calls
-        # embedded deeper in the hierarchy and assert that the call
-        # hierarchy is preserved as we expect it
+        # Check if get_selected_row was called on dataset_list_box
+        mock_dataset_list_box.get_selected_row.assert_called_once()
+
+
+from unittest.mock import AsyncMock
+
+@pytest.mark.asyncio
+async def test_do_get_item(app):
+    """Test the do_get_item method for copying a selected item."""
+
+    # Mock dependencies
+    mock_dataset_list_box = MagicMock()
+    app.main_window.dataset_list_box = mock_dataset_list_box
+    mock_settings = MagicMock()
+    app.main_window.settings = mock_settings
+
+    # Mock _get_selected_items to return one item
+    app.main_window._get_selected_items = MagicMock(return_value=[('item_name', 'item_uuid')])
+
+    # Create a mock action and variant
+    mock_action = MagicMock(spec=Gio.SimpleAction)
+    mock_variant = GLib.Variant.new_string("dummy_path")
+
+    # Mock async call in do_get_item
+    mock_dataset = MagicMock()
+    mock_dataset.get_item = AsyncMock()  # Use AsyncMock for async calls
+    mock_dataset_list_box.get_selected_row.return_value = MagicMock(dataset=mock_dataset)
+
+    # Call the method with mock objects
+    app.main_window.do_get_item(mock_action, mock_variant)
+    # Use patch.object to mock do_get_item method
+
+
