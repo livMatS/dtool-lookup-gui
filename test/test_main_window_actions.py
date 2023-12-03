5ad23f04fab8d3c1ebbe957c35e43713076c5d6d
ashdriod <ashwinvazhappilly@gmail.com>
2023-12-03 17:41:13 +0100

    Action Testing

--- test/test_main_window_actions.py
+++ test/test_main_window_actions.py
@@ -95,3 +95,100 @@ async def test_do_get_item_action_trigger(app):
         mock_do_get_item.assert_called_once_with(get_item_action, dest_file_variant)
 
 
+@pytest.mark.asyncio
+async def test_do_search_select_and_show_direct_call(app):
+    """Test the do_search_select_and_show method for processing a search directly."""
+
+    # Mock dependencies
+    mock_base_uri_list_box = MagicMock()
+    app.main_window.base_uri_list_box = mock_base_uri_list_box
+
+    # Mock _search_select_and_show to simulate search behavior
+    app.main_window._search_select_and_show = MagicMock()
+
+    # Create a mock action and variant
+    mock_action = MagicMock(spec=Gio.SimpleAction)
+    mock_variant = GLib.Variant.new_string("test_search_query")
+
+    # Directly call the method with mock objects
+    app.main_window.do_search_select_and_show(mock_action, mock_variant)
+
+    # Assert that _search_select_and_show was called with the correct query
+    app.main_window._search_select_and_show.assert_called_once_with("test_search_query")
+
+
+@pytest.mark.asyncio
+async def test_do_search_select_and_show_action_trigger(app):
+    """Test if 'search-select-show' action triggers do_search_select_and_show method."""
+
+    # Mock dependencies
+    mock_base_uri_list_box = MagicMock()
+    app.main_window.base_uri_list_box = mock_base_uri_list_box
+
+    # Setup necessary mocks for the action trigger
+    app.main_window._search_select_and_show = MagicMock()
+
+    # Create and add the action
+    search_variant = GLib.Variant.new_string("test_search_query")
+    search_select_show_action = Gio.SimpleAction.new("search-select-show", search_variant.get_type())
+    app.main_window.add_action(search_select_show_action)
+
+    # Patch do_search_select_and_show method after action is added
+    with patch.object(app.main_window, 'do_search_select_and_show', new_callable=MagicMock) as mock_do_search_select_and_show:
+        # Connect the action
+        search_select_show_action.connect("activate", app.main_window.do_search_select_and_show)
+
+        # Trigger the action
+        search_select_show_action.activate(search_variant)
+
+        # Assert that do_search_select_and_show was called once
+        mock_do_search_select_and_show.assert_called_once_with(search_select_show_action, search_variant)
+
+@pytest.mark.asyncio
+async def test_do_show_dataset_details_by_uri_direct_call(app):
+    """Test the do_show_dataset_details_by_uri method for showing dataset details directly."""
+
+    # Mock dependencies
+    mock_dataset_list_box = MagicMock()
+    app.main_window.dataset_list_box = mock_dataset_list_box
+
+    # Mock _show_dataset_details_by_uri to simulate behavior
+    app.main_window._show_dataset_details_by_uri = MagicMock()
+
+    # Create a mock action and variant
+    mock_action = MagicMock(spec=Gio.SimpleAction)
+    mock_variant = GLib.Variant.new_string("dummy_uri")
+
+    # Directly call the method with mock objects
+    app.main_window.do_show_dataset_details_by_uri(mock_action, mock_variant)
+
+    # Assert that _show_dataset_details_by_uri was called with the correct uri
+    app.main_window._show_dataset_details_by_uri.assert_called_once_with("dummy_uri")
+
+
+@pytest.mark.asyncio
+async def test_do_show_dataset_details_by_uri_action_trigger(app):
+    """Test if 'show-dataset-by-uri' action triggers do_show_dataset_details_by_uri method."""
+
+    # Mock dependencies
+    mock_dataset_list_box = MagicMock()
+    app.main_window.dataset_list_box = mock_dataset_list_box
+
+    # Setup necessary mocks for the action trigger
+    app.main_window._show_dataset_details_by_uri = MagicMock()
+
+    # Create and add the action
+    uri_variant = GLib.Variant.new_string("dummy_uri")
+    show_dataset_by_uri_action = Gio.SimpleAction.new("show-dataset-by-uri", uri_variant.get_type())
+    app.main_window.add_action(show_dataset_by_uri_action)
+
+    # Patch do_show_dataset_details_by_uri method after action is added
+    with patch.object(app.main_window, 'do_show_dataset_details_by_uri', new_callable=MagicMock) as mock_do_show_dataset_details_by_uri:
+        # Connect the action
+        show_dataset_by_uri_action.connect("activate", app.main_window.do_show_dataset_details_by_uri)
+
+        # Trigger the action
+        show_dataset_by_uri_action.activate(uri_variant)
+
+        # Assert that do_show_dataset_details_by_uri was called once
+        mock_do_show_dataset_details_by_uri.assert_called_once_with(show_dataset_by_uri_action, uri_variant)
