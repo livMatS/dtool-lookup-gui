77290c066ea91eb08b96d79502d45c8c8a9c377d
Johannes L. Hörmann <johannes.laurin@gmail.com>
2023-12-04 14:50:54 +0100

    Revert "Unit tests for main window actions"

--- test/conftest.py
+++ test/conftest.py
@@ -1,30 +1,19 @@
-import json
 import logging
-import os
 import pytest
-
-from unittest.mock import patch
 
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('GtkSource', '4')
 from gi.repository import GLib, GObject, Gio, Gtk, GtkSource, GdkPixbuf
 
-import asyncio
 import gbulb
 gbulb.install(gtk=True)
 
-import dtool_lookup_api.core.LookupClient
-
 from dtool_lookup_gui.main import Application
-from dtool_lookup_gui.views.main_window import MainWindow
 
 
 logger = logging.getLogger(__name__)
 
-
-_HERE = os.path.dirname(os.path.abspath(__file__))
-# _ROOT = os.path.join(_HERE, "..")
 
 # ==========================================================================
 # fixtures from https://github.com/beeware/gbulb/blob/main/tests/conftest.py
@@ -86,62 +75,6 @@ def gtk_loop(gtk_policy):
 def app(gtk_loop):
     app = Application(loop=gtk_loop)
     yield app
-
-@pytest.fixture(scope="function")
-def mock_token(scope="function"):
-    """Provide a mock authentication token"""
-    yield "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwMTY4OTU5MywianRpIjoiYTdkN2Y5ZWItZGI3MS00YjExLWFhMzktZGQ2YzgzOTJmOWE4IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InRlc3R1c2VyIiwibmJmIjoxNzAxNjg5NTkzLCJleHAiOjE3MDE2OTMxOTN9.t9SQ00ecZRc-pspz-Du7321xfWIzgTTFKobkNed1CuQYHvtNrc3vdHYbqCWaYCqZpEVF8RlltldT4Lookx6vNgnW4olpiS2KTZ-X2asMhn7SShDtUJuU54CGeViWzYX_V_Pzckoe_cgjFkOutRvnwy_072Whnmc0TwYojwNqUScAIJRu0pzym84JngloXfdI7r25GcRVNtzsGUl7DDfrIz4aSOeVDAVEXhPjgEatKsvNdVZl1DIJsTZpuI7Jh7ZW1WsyjonqHR0J0kIVQn9imQyLyS9_CtmURBQ3kabx6cxhpx5LADrzLutSu24eA4FyECOdzjJ3SPGb9nIVTEDxQg"
-
-
-@pytest.fixture(scope="function")
-def mock_dataset_list():
-    """Provide a mock dataset list"""
-    with open(os.path.join(_HERE, 'data', 'mock_dataset_search_response.json'), 'r') as f:
-        dataset_list = json.load(f)
-
-    yield dataset_list
-
-
-@pytest.fixture(scope="function")
-def mock_config_info():
-    """Provide a mock server config info"""
-    with open(os.path.join(_HERE, 'data', 'mock_config_info_response.json'), 'r') as f:
-        config_info = json.load(f)
-
-    yield config_info
-
-
-@pytest.fixture(scope="function")
-def app_without_authentication(app, mock_token):
-    """Removes authentication from app."""
-    with patch("dtool_lookup_api.core.LookupClient.authenticate", return_value=mock_token):
-        yield app
-
-
-@pytest.fixture(scope="function")
-def app_with_mock_dtool_lookup_api_calls(
-        app_without_authentication, mock_dataset_list, mock_config_info):
-    """Replaces lookup api calls with mock methods that return fake lists of datasets."""
-
-    # TODO: figure out whether mocked methods work as they should
-    with (
-            patch("dtool_lookup_api.core.LookupClient.TokenBasedLookupClient.all", return_value=mock_dataset_list),
-            patch("dtool_lookup_api.core.LookupClient.TokenBasedLookupClient.search", return_value=mock_dataset_list),
-            patch("dtool_lookup_api.core.LookupClient.TokenBasedLookupClient.config", return_value=mock_config_info),
-            patch("dtool_lookup_api.core.LookupClient.ConfigurationBasedLookupClient.has_valid_token", return_value=True)
-        ):
-
-        yield app_without_authentication
-
-
-@pytest.fixture(scope="function")
-def populated_app(app_with_mock_dtool_lookup_api_calls):
-    """Provides app populated with mock datasets."""
-
-    # TODO: figure out how to populate and refresh app with data here before app launched
-    # app.main_window.activate_action('refresh-view')
-
-    yield app_with_mock_dtool_lookup_api_calls
 
 
 # ================================================================
