112e06860d49f6ac81e3d63f47f4d17a1a185a26
ashdriod <ashwinvazhappilly@gmail.com>
2023-12-16 22:06:41 +0100

    Final Commit

--- test/test_main_window_actions.py
+++ test/test_main_window_actions.py
@@ -7,7 +7,7 @@ import asyncio
 import time
 
 import pytest
-from unittest.mock import patch, MagicMock, AsyncMock
+from unittest.mock import patch, MagicMock, AsyncMock, Mock
 from gi.repository import Gtk, Gio, GLib
 
 
@@ -31,6 +31,158 @@ async def test_refresh_method_triggered_by_action(app):
         # Assert that the refresh method was called once
         mock_refresh.assert_called_once()
 
+@pytest.mark.asyncio
+async def test_do_search_select_and_show_direct_call(populated_app, mock_dataset_list):
+    """
+    Test the do_search_select_and_show method for processing a search directly. It verifies
+    if the dataset list is correctly populated, the first dataset is selected, and its
+    content is displayed.
+    """
+
+    async def wait_for_datasets_to_load(list_box, timeout=10):
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            if len(list_box.get_children()) > 0:
+                return True  # Datasets are loaded
+            await asyncio.sleep(0.1)  # Yield control to allow other async tasks to run
+        return False  # Timeout reached
+
+    # Trigger the 'refresh-view' action
+    populated_app.main_window.activate_action('refresh-view')
+
+    # Wait until datasets are loaded
+    datasets_loaded = await wait_for_datasets_to_load(populated_app.main_window.dataset_list_box)
+    assert datasets_loaded, "Datasets were not loaded in time"
+
+    # Create a GLib.Variant with the test search query
+    mock_variant = GLib.Variant.new_string("test_search_query")
+
+    # Call the method with the test search query
+    populated_app.main_window.do_search_select_and_show(None, mock_variant)
+
+    # Assertions to check the state of the application after the search
+    assert len(populated_app.main_window.dataset_list_box.get_children()) > 0, "No datasets found in the list box"
+    first_dataset_row = populated_app.main_window.dataset_list_box.get_children()[0]
+    dataset = first_dataset_row.dataset
+
+    # Assert that the dataset's URI matches the expected URI
+    assert dataset.uri == mock_dataset_list[0]['uri'], f"Expected URI {mock_dataset_list[0]['uri']}, got {dataset.uri}"
+
+    # Await completion of all tasks related to the test
+    pending_tasks = [task for task in asyncio.all_tasks() if task is not asyncio.current_task()]
+    if pending_tasks:
+        await asyncio.gather(*pending_tasks)
+
+
+@pytest.mark.asyncio
+async def test_do_search_select_and_show_action_trigger(app):
+    """Test if 'search-select-show' action triggers do_search_select_and_show method."""
+
+    # Mock dependencies
+    mock_base_uri_list_box = MagicMock()
+    app.main_window.base_uri_list_box = mock_base_uri_list_box
+
+    # Setup necessary mocks for the action trigger
+    app.main_window._search_select_and_show = MagicMock()
+
+    # Create and add the action
+    search_variant = GLib.Variant.new_string("test_search_query")
+    search_select_show_action = Gio.SimpleAction.new("search-select-show", search_variant.get_type())
+    app.main_window.add_action(search_select_show_action)
+
+    # Patch do_search_select_and_show method after action is added
+    with patch.object(app.main_window, 'do_search_select_and_show',
+                      new_callable=MagicMock) as mock_do_search_select_and_show:
+        # Connect the action
+        search_select_show_action.connect("activate", app.main_window.do_search_select_and_show)
+
+        # Trigger the action
+        search_select_show_action.activate(search_variant)
+
+        # Assert that do_search_select_and_show was called once
+        mock_do_search_select_and_show.assert_called_once_with(search_select_show_action, search_variant)
+
+@pytest.mark.asyncio
+async def test_do_show_dataset_details_by_uri_direct_call(populated_app):
+    """
+    Test the do_show_dataset_details_by_uri method for processing a URI directly. It verifies
+    if the correct dataset is selected and its content is displayed based on the URI.
+    """
+
+    async def wait_for_datasets_to_load(list_box, timeout=10):
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            if len(list_box.get_children()) > 0:
+                return True  # Datasets are loaded
+            await asyncio.sleep(0.1)  # Yield control to allow other async tasks to run
+        return False  # Timeout reached
+
+    # Trigger the 'refresh-view' action
+    populated_app.main_window.activate_action('refresh-view')
+
+    # Wait until datasets are loaded
+    datasets_loaded = await wait_for_datasets_to_load(populated_app.main_window.dataset_list_box)
+    assert datasets_loaded, "Datasets were not loaded in time"
+
+    # Define a URI that corresponds to a dataset in the populated dataset list
+    test_uri = "smb://test-share/51e67a6e-3f38-43e9-bb79-01700dc09c61"
+
+    # Create a GLib.Variant with the test URI
+    uri_variant = GLib.Variant.new_string(test_uri)
+
+    # Call the do_show_dataset_details_by_uri method with the test URI
+    populated_app.main_window.do_show_dataset_details_by_uri(None, uri_variant)
+
+    # Retrieve the dataset that should be selected based on the URI
+    index = populated_app.main_window.dataset_list_box.get_row_index_from_uri(test_uri)
+    selected_row = populated_app.main_window.dataset_list_box.get_row_at_index(index)
+    selected_dataset = selected_row.dataset if selected_row is not None else None
+
+    # Assert that the dataset with the given URI is selected
+    assert selected_dataset is not None, "No dataset was selected"
+    assert selected_dataset.uri == test_uri, f"Expected URI {test_uri}, got {selected_dataset.uri}"
+
+    # Await completion of all tasks related to the test
+    pending_tasks = [task for task in asyncio.all_tasks() if task is not asyncio.current_task()]
+    if pending_tasks:
+        await asyncio.gather(*pending_tasks)
+
+
+@pytest.mark.asyncio
+async def test_do_show_dataset_details_by_uri_action_trigger(app):
+    """Test if 'show-dataset-by-uri' action triggers do_show_dataset_details_by_uri method."""
+
+    # Mock dependencies
+    mock_dataset_list_box = MagicMock()
+    app.main_window.dataset_list_box = mock_dataset_list_box
+
+    # Setup necessary mocks for the action trigger
+    app.main_window._show_dataset_details_by_uri = MagicMock()
+
+    # Create and add the action
+    uri_variant = GLib.Variant.new_string("dummy_uri")
+    show_dataset_by_uri_action = Gio.SimpleAction.new("show-dataset-by-uri", uri_variant.get_type())
+    app.main_window.add_action(show_dataset_by_uri_action)
+
+    # Patch do_show_dataset_details_by_uri method after action is added
+    with patch.object(app.main_window, 'do_show_dataset_details_by_uri',
+                      new_callable=MagicMock) as mock_do_show_dataset_details_by_uri:
+        # Connect the action
+        show_dataset_by_uri_action.connect("activate", app.main_window.do_show_dataset_details_by_uri)
+
+        # Trigger the action
+        show_dataset_by_uri_action.activate(uri_variant)
+
+        # Assert that do_show_dataset_details_by_uri was called once
+        mock_do_show_dataset_details_by_uri.assert_called_once_with(show_dataset_by_uri_action, uri_variant)
+
+
+
+
+
+
+
+
 
 @pytest.mark.asyncio
 async def test_do_get_item_direct_call_fails_due_to_no_selected_item(app):
@@ -52,7 +204,6 @@ async def test_populate_dataset_list(populated_app):
     of the application in a test environment.
     """
 
-
     # Call the activate_action method to trigger the 'refresh-view' action
     populated_app.main_window.activate_action('refresh-view')
 
@@ -60,32 +211,70 @@ async def test_populate_dataset_list(populated_app):
     await asyncio.sleep(500)
 
 
+
 @pytest.mark.asyncio
 @pytest.mark.skip(reason="no way of currently testing this")
-async def test_do_get_item_direct_call(app):
-    """Test the do_get_item method for copying a selected item directly."""
+async def test_do_get_item_direct_call(populated_app, tmp_path):
+    """
+    Test the do_get_item method for copying a selected item directly.
+    It verifies if the selected item is correctly copied to the specified destination.
+    """
 
-    # Mock dependencies
-    # mock_dataset_list_box = MagicMock()
-    # app.main_window.dataset_list_box = mock_dataset_list_box
-    # mock_settings = MagicMock()
-    # app.main_window.settings = mock_settings
+    async def wait_for_datasets_to_load(list_box, timeout=10):
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            if len(list_box.get_children()) > 0:
+                return True  # Datasets are loaded
+            await asyncio.sleep(0.1)  # Yield control to allow other async tasks to run
+        return False  # Timeout reached
 
-    # Mock _get_selected_items to return one item
-    # app.main_window._get_selected_items = MagicMock(return_value=[('item_name', 'item_uuid')])
+    # Trigger the 'refresh-view' action to load datasets
+    populated_app.main_window.activate_action('refresh-view')
 
-    # Create a mock action and variant
-    # mock_action = MagicMock(spec=Gio.SimpleAction)
-    mock_variant = GLib.Variant.new_string("dummy_path")
+    # Wait until datasets are loaded
+    datasets_loaded = await wait_for_datasets_to_load(populated_app.main_window.dataset_list_box)
+    assert datasets_loaded, "Datasets were not loaded in time"
 
-    # Mock async call in do_get_item
-    # mock_dataset = MagicMock()
-    # mock_dataset.get_item = AsyncMock()
-    # mock_dataset_list_box.get_selected_row.return_value = MagicMock(dataset=mock_dataset)
+    # Select the first dataset
+    first_dataset_row = populated_app.main_window.dataset_list_box.get_children()[0]
+    dataset = first_dataset_row.dataset
 
-    # Directly call the method with mock objects
-    with pytest.raises(AttributeError, match="'NoneType' object has no attribute 'dataset'"):
-        app.main_window.do_get_item(None, mock_variant)
+    # Assuming the first dataset's UUID as the item UUID (Replace this with the actual item UUID logic)
+    item_uuid = dataset.uuid
+    populated_app.main_window._get_selected_items = lambda: [('item_name', item_uuid)]
+
+    # Define the destination file path
+    dest_file = tmp_path / "destination_file"
+
+    # Create a GLib.Variant with the destination file path
+    dest_file_variant = GLib.Variant.new_string(str(dest_file))
+
+    dest_file = tmp_path / "destination_file"
+    print(f"Destination file path: {dest_file}")
+
+    # Call the do_get_item method without 'await'
+    try:
+        populated_app.main_window.do_get_item(None, dest_file_variant)
+    except Exception as e:
+        print(f"Error during 'do_get_item': {e}")
+        raise
+
+    # Wait for all asynchronous tasks to complete
+    await asyncio.sleep(1)  # Increase sleep time if necessary
+    pending_tasks = [task for task in asyncio.all_tasks() if task is not asyncio.current_task()]
+    if pending_tasks:
+        completed_tasks = await asyncio.gather(*pending_tasks, return_exceptions=True)
+        for task in completed_tasks:
+            if isinstance(task, Exception):
+                print(f"Exception in async task: {task}")
+
+    # Verify that the file was copied
+    file_exists = dest_file.exists()
+    print(f"File exists: {file_exists}")
+    assert file_exists, "The item was not copied to the specified destination"
+
+
+
 
 
 @pytest.mark.asyncio
@@ -120,140 +309,9 @@ async def test_do_get_item_action_trigger(app):
         # Assert that do_get_item was called once
         mock_do_get_item.assert_called_once_with(get_item_action, dest_file_variant)
 
-# TODO: let's start with this unit est and transform it into a proper test on GUI behavior
-@pytest.mark.asyncio
-async def test_do_search_select_and_show_direct_call(populated_app, mock_dataset_list):
-    """
-    Test the do_search_select_and_show method for processing a search directly. It verifies
-    if the dataset list is correctly populated, the first dataset is selected, and its
-    content is displayed.
-    """
-
-    # TODO: assert that
-    # * dataset list is populated with information returned by dtool_lookup_api.search
-    # * first dataset in list is selected in the GUI
-    # * content of dataset is displayed on the right hand side
-    # --> need to figure out how to inspect the GUI elements
-
-    # Assert that _search_select_and_show was called with the correct query
-    # populated_app.main_window._search_select_and_show.assert_called_once_with("test_search_query")
-
-    async def wait_for_datasets_to_load(list_box, timeout=10):
-        start_time = time.time()
-        while time.time() - start_time < timeout:
-            if len(list_box.get_children()) > 0:
-                return True  # Datasets are loaded
-            await asyncio.sleep(0.1)  # Short sleep to yield control and wait
-        return False  # Timeout reached
-
-    # Trigger the 'refresh-view' action
-    populated_app.main_window.activate_action('refresh-view')
-
-    # Wait until datasets are loaded
-    datasets_loaded = await wait_for_datasets_to_load(populated_app.main_window.dataset_list_box)
-    assert datasets_loaded, "Datasets were not loaded in time"
-
-    # Create a GLib.Variant with the test search query
-    mock_variant = GLib.Variant.new_string("test_search_query")
-
-    # Call the method with the test search query
-    populated_app.main_window.do_search_select_and_show(None, mock_variant)
-
-    # Assertions to check the state of the application after the search
-    assert len(populated_app.main_window.dataset_list_box.get_children()) > 0
-    first_dataset_row = populated_app.main_window.dataset_list_box.get_children()[0]
-
-    # Access the dataset from the first row
-    dataset = first_dataset_row.dataset
-
-    # Assert that the dataset's URI matches the first item in your mock dataset list
-    assert dataset.uri == mock_dataset_list[0]['uri']
-
-    #print("dataset.uri: ", dataset.uri)
-
-    # Need to await not-yet-finished tasks, otherwise will fail with
-    #     Failed: Task was destroyed but it is pending!
-    await asyncio.sleep(1)
 
 
-@pytest.mark.asyncio
-async def test_do_search_select_and_show_action_trigger(app):
-    """Test if 'search-select-show' action triggers do_search_select_and_show method."""
 
-    # Mock dependencies
-    mock_base_uri_list_box = MagicMock()
-    app.main_window.base_uri_list_box = mock_base_uri_list_box
-
-    # Setup necessary mocks for the action trigger
-    app.main_window._search_select_and_show = MagicMock()
-
-    # Create and add the action
-    search_variant = GLib.Variant.new_string("test_search_query")
-    search_select_show_action = Gio.SimpleAction.new("search-select-show", search_variant.get_type())
-    app.main_window.add_action(search_select_show_action)
-
-    # Patch do_search_select_and_show method after action is added
-    with patch.object(app.main_window, 'do_search_select_and_show',
-                      new_callable=MagicMock) as mock_do_search_select_and_show:
-        # Connect the action
-        search_select_show_action.connect("activate", app.main_window.do_search_select_and_show)
-
-        # Trigger the action
-        search_select_show_action.activate(search_variant)
-
-        # Assert that do_search_select_and_show was called once
-        mock_do_search_select_and_show.assert_called_once_with(search_select_show_action, search_variant)
-
-
-@pytest.mark.asyncio
-async def test_do_show_dataset_details_by_uri_direct_call(app):
-    """Test the do_show_dataset_details_by_uri method for showing dataset details directly."""
-
-    # Mock dependencies
-    mock_dataset_list_box = MagicMock()
-    app.main_window.dataset_list_box = mock_dataset_list_box
-
-    # Mock _show_dataset_details_by_uri to simulate behavior
-    app.main_window._show_dataset_details_by_uri = MagicMock()
-
-    # Create a mock action and variant
-    mock_action = MagicMock(spec=Gio.SimpleAction)
-    mock_variant = GLib.Variant.new_string("dummy_uri")
-
-    # Directly call the method with mock objects
-    app.main_window.do_show_dataset_details_by_uri(mock_action, mock_variant)
-
-    # Assert that _show_dataset_details_by_uri was called with the correct uri
-    app.main_window._show_dataset_details_by_uri.assert_called_once_with("dummy_uri")
-
-
-@pytest.mark.asyncio
-async def test_do_show_dataset_details_by_uri_action_trigger(app):
-    """Test if 'show-dataset-by-uri' action triggers do_show_dataset_details_by_uri method."""
-
-    # Mock dependencies
-    mock_dataset_list_box = MagicMock()
-    app.main_window.dataset_list_box = mock_dataset_list_box
-
-    # Setup necessary mocks for the action trigger
-    app.main_window._show_dataset_details_by_uri = MagicMock()
-
-    # Create and add the action
-    uri_variant = GLib.Variant.new_string("dummy_uri")
-    show_dataset_by_uri_action = Gio.SimpleAction.new("show-dataset-by-uri", uri_variant.get_type())
-    app.main_window.add_action(show_dataset_by_uri_action)
-
-    # Patch do_show_dataset_details_by_uri method after action is added
-    with patch.object(app.main_window, 'do_show_dataset_details_by_uri',
-                      new_callable=MagicMock) as mock_do_show_dataset_details_by_uri:
-        # Connect the action
-        show_dataset_by_uri_action.connect("activate", app.main_window.do_show_dataset_details_by_uri)
-
-        # Trigger the action
-        show_dataset_by_uri_action.activate(uri_variant)
-
-        # Assert that do_show_dataset_details_by_uri was called once
-        mock_do_show_dataset_details_by_uri.assert_called_once_with(show_dataset_by_uri_action, uri_variant)
 
 
 @pytest.mark.asyncio
