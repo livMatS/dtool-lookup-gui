d26a1d0991ec76b76a95a7047da590cbe57cfcf8
ashdriod <ashwinvazhappilly@gmail.com>
2023-12-17 03:12:32 +0100

    TST:WIP

--- test/test_main_window_actions.py
+++ test/test_main_window_actions.py
@@ -11,7 +11,6 @@ from unittest.mock import patch, MagicMock, AsyncMock, Mock
 from gi.repository import Gtk, Gio, GLib
 
 
-
 @pytest.mark.asyncio
 async def test_do_search_direct_call(populated_app):
     """
@@ -63,6 +62,7 @@ async def test_do_search_direct_call(populated_app):
     if pending_tasks:
         await asyncio.gather(*pending_tasks)
 
+
 @pytest.mark.asyncio
 async def test_do_search_action_trigger(app):
     """Test if 'search' action triggers do_search method."""
@@ -89,6 +89,7 @@ async def test_do_search_action_trigger(app):
 
         # Assert that do_search was called once
         mock_do_search.assert_called_once_with(search_action, search_text_variant)
+
 
 @pytest.mark.asyncio
 async def test_do_select_dataset_row_by_row_index_direct_call(populated_app):
@@ -138,6 +139,7 @@ async def test_do_select_dataset_row_by_row_index_direct_call(populated_app):
     pending_tasks = [task for task in asyncio.all_tasks() if task is not asyncio.current_task()]
     if pending_tasks:
         await asyncio.gather(*pending_tasks)
+
 
 @pytest.mark.asyncio
 async def test_do_select_dataset_row_by_row_index_action_trigger(app):
@@ -212,6 +214,7 @@ async def test_do_show_dataset_details_by_uri_direct_call(populated_app):
     pending_tasks = [task for task in asyncio.all_tasks() if task is not asyncio.current_task()]
     if pending_tasks:
         await asyncio.gather(*pending_tasks)
+
 
 @pytest.mark.asyncio
 async def test_do_show_dataset_details_by_uri_action_trigger(app):
@@ -397,95 +400,6 @@ async def test_do_search_select_and_show_action_trigger(app):
         mock_do_search_select_and_show.assert_called_once_with(search_select_show_action, search_variant)
 
 
-
-
-
-
-
-@pytest.mark.asyncio
-async def test_do_get_item_action_trigger(app):
-    """Test if 'get-item' action triggers do_get_item method."""
-
-    # Mock dependencies
-    mock_dataset_list_box = MagicMock()
-    app.main_window.dataset_list_box = mock_dataset_list_box
-    mock_settings = MagicMock()
-    app.main_window.settings = mock_settings
-
-    # Setup necessary mocks for the action trigger
-    mock_dataset = MagicMock()
-    mock_dataset.get_item = AsyncMock()
-    mock_dataset_list_box.get_selected_row.return_value = MagicMock(dataset=mock_dataset)
-    app.main_window._get_selected_items = MagicMock(return_value=[('item_name', 'item_uuid')])
-
-    # Create and add the action
-    dest_file_variant = GLib.Variant.new_string("dummy_path")
-    get_item_action = Gio.SimpleAction.new("get-item", dest_file_variant.get_type())
-    app.main_window.add_action(get_item_action)
-
-    # Patch do_get_item method after action is added
-    with patch.object(app.main_window, 'do_get_item', new_callable=MagicMock) as mock_do_get_item:
-        # Connect the action
-        get_item_action.connect("activate", app.main_window.do_get_item)
-
-        # Trigger the action
-        get_item_action.activate(dest_file_variant)
-
-        # Assert that do_get_item was called once
-        mock_do_get_item.assert_called_once_with(get_item_action, dest_file_variant)
-
-
-@pytest.mark.asyncio
-async def test_do_refresh_view_direct_call(app):
-    """Test the direct call of the do_refresh_view method."""
-
-    # Directly call the do_refresh_view method
-    app.main_window.do_refresh_view(None, None)
-
-@pytest.mark.asyncio
-async def test_refresh_method_triggered_by_action(app):
-    """Test if the 'refresh-view' action triggers the refresh method."""
-
-    # Patch the main window's refresh method
-    with patch.object(app.main_window, 'refresh', new_callable=MagicMock) as mock_refresh:
-        # Trigger the 'refresh-view' action
-        app.main_window.activate_action('refresh-view')
-
-        # Assert that the refresh method was called once
-        mock_refresh.assert_called_once()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@pytest.mark.asyncio
-async def test_do_get_item_direct_call_fails_due_to_no_selected_item(app):
-    """Test that the do_get_item method for copying a selected item fails when not item is selected."""
-
-    mock_variant = GLib.Variant.new_string("dummy_path")
-
-    # Directly call the method with mock objects
-    with pytest.raises(AttributeError, match="'NoneType' object has no attribute 'dataset'"):
-        app.main_window.do_get_item(None, mock_variant)
-
-
-
-
 @pytest.mark.asyncio
 @pytest.mark.skip(reason="no way of currently testing this")
 async def test_do_get_item_direct_call(populated_app, tmp_path):
@@ -548,23 +462,66 @@ async def test_do_get_item_direct_call(populated_app, tmp_path):
     assert file_exists, "The item was not copied to the specified destination"
 
 
+@pytest.mark.asyncio
+async def test_do_get_item_action_trigger(app):
+    """Test if 'get-item' action triggers do_get_item method."""
+
+    # Mock dependencies
+    mock_dataset_list_box = MagicMock()
+    app.main_window.dataset_list_box = mock_dataset_list_box
+    mock_settings = MagicMock()
+    app.main_window.settings = mock_settings
+
+    # Setup necessary mocks for the action trigger
+    mock_dataset = MagicMock()
+    mock_dataset.get_item = AsyncMock()
+    mock_dataset_list_box.get_selected_row.return_value = MagicMock(dataset=mock_dataset)
+    app.main_window._get_selected_items = MagicMock(return_value=[('item_name', 'item_uuid')])
+
+    # Create and add the action
+    dest_file_variant = GLib.Variant.new_string("dummy_path")
+    get_item_action = Gio.SimpleAction.new("get-item", dest_file_variant.get_type())
+    app.main_window.add_action(get_item_action)
+
+    # Patch do_get_item method after action is added
+    with patch.object(app.main_window, 'do_get_item', new_callable=MagicMock) as mock_do_get_item:
+        # Connect the action
+        get_item_action.connect("activate", app.main_window.do_get_item)
+
+        # Trigger the action
+        get_item_action.activate(dest_file_variant)
+
+        # Assert that do_get_item was called once
+        mock_do_get_item.assert_called_once_with(get_item_action, dest_file_variant)
 
 
+@pytest.mark.asyncio
+async def test_do_refresh_view_direct_call(app):
+    """Test the direct call of the do_refresh_view method."""
+
+    # Directly call the do_refresh_view method
+    app.main_window.do_refresh_view(None, None)
 
 
+@pytest.mark.asyncio
+async def test_refresh_method_triggered_by_action(app):
+    """Test if the 'refresh-view' action triggers the refresh method."""
+
+    # Patch the main window's refresh method
+    with patch.object(app.main_window, 'refresh', new_callable=MagicMock) as mock_refresh:
+        # Trigger the 'refresh-view' action
+        app.main_window.activate_action('refresh-view')
+
+        # Assert that the refresh method was called once
+        mock_refresh.assert_called_once()
 
 
+@pytest.mark.asyncio
+async def test_do_get_item_direct_call_fails_due_to_no_selected_item(app):
+    """Test that the do_get_item method for copying a selected item fails when not item is selected."""
 
+    mock_variant = GLib.Variant.new_string("dummy_path")
 
-
-
-
-
-
-
-
-
-
-
-
-
+    # Directly call the method with mock objects
+    with pytest.raises(AttributeError, match="'NoneType' object has no attribute 'dataset'"):
+        app.main_window.do_get_item(None, mock_variant)
