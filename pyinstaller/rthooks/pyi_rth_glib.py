636e2c8e008bf1b9330e7d26135d932e1bc01783
Johannes Laurin Hoermann <hoermann@melius.icts.nagoya-u.ac.jp>
2026-03-26 21:36:09 +0900

    BUG: set GI_TYPELIB_PATH in runtime hook so bundled app finds typelibs
    Without GI_TYPELIB_PATH pointing at the bundled gi_typelibs/ directory,
    gi.repository raises 'Typelib file for namespace Gtk version 3.0 not found'
    at startup. The bundled executable is effectively defunct without this fix.

--- pyinstaller/rthooks/pyi_rth_glib.py
+++ pyinstaller/rthooks/pyi_rth_glib.py
@@ -23,3 +23,16 @@ if xdg_data_dirs_orig is not None:
     os.environ['XDG_DATA_DIRS'] = xdg_data_dirs_prepend + os.pathsep + xdg_data_dirs_orig
 else:
     os.environ['XDG_DATA_DIRS'] = xdg_data_dirs_prepend
+
+# Set GI_TYPELIB_PATH so gi.repository can find the bundled typelib files.
+# PyInstaller bundles typelibs into the gi_typelibs/ subdirectory of _MEIPASS.
+# Without this, the bundled app raises:
+#   gi.RepositoryError: Typelib file for namespace 'Gtk', version '3.0' not found
+gi_typelib_path = os.path.join(sys._MEIPASS, 'gi_typelibs')
+gi_typelib_path_orig = os.environ.get("GI_TYPELIB_PATH", None)
+
+if os.path.isdir(gi_typelib_path):
+    if gi_typelib_path_orig is not None:
+        os.environ['GI_TYPELIB_PATH'] = gi_typelib_path + os.pathsep + gi_typelib_path_orig
+    else:
+        os.environ['GI_TYPELIB_PATH'] = gi_typelib_path
